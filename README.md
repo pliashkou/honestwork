f581d853e5671f80380dce7957d86662730a9547
Yury Pliashkou <pliashkou@gmail.com>
2020-06-23 00:45:30 +0300

    Inital working project

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# honestwork
+
+##Installation
+
+```shell script
+composer install
+yarn install
+```
+
+###Backend
+
+```shell script
+symfony server:start
+```
+
+###Frontend
+
+```shell script
+yarn dev
+```
---
+++ assets/js/App.tsx
@@ -0,0 +1,15 @@
+import '../css/app.scss';
+
+import React from 'react';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        Hello world
+      </header>
+    </div>
+  );
+}
+
+export default App;
