59cb59c86e1cd1ae0c10e753d45a79180fcb1823
Yury Pliashkou <pliashkou@gmail.com>
2020-06-22 21:48:31 +0300

    added frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          I lost my job and need help, <br/>
+          I want to help with development.<br/>
+          I want to help with work <br/>
+          I want to hire someone.
+
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
